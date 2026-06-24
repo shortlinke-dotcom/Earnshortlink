05d37b6c0f8b42a5e7e3680efe7bc0ebd44fa96b
shortlinke-dotcom <shortlinke@gmail.com>
2026-06-25 04:16:43 +0700

    Create models.py

---
+++ models.py
@@ -0,0 +1,10 @@
+from database import Base
+from sqlalchemy import Column, Integer, String
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    gmail = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String)
