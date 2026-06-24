0b97d3a2ed457e3f0958377642a498c554b4aeeb
shortlinke-dotcom <shortlinke@gmail.com>
2026-06-25 01:58:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,23 +6,26 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
+        request=request,
+        name="index.html"
     )
+
 
 @app.get("/login", response_class=HTMLResponse)
 async def login(request: Request):
     return templates.TemplateResponse(
-        "login.html",
-        {"request": request}
+        request=request,
+        name="login.html"
     )
+
 
 @app.get("/register", response_class=HTMLResponse)
 async def register(request: Request):
     return templates.TemplateResponse(
-        "register.html",
-        {"request": request}
+        request=request,
+        name="register.html"
     )
