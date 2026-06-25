95fb9a82d8e4f72d90425913d34fe3eccf8ceef8
shortlinke-dotcom <shortlinke@gmail.com>
2026-06-25 09:13:03 +0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,9 +1,16 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+import bcrypt
 
 def hash_password(password: str):
-    return pwd_context.hash(password)
+    return bcrypt.hashpw(
+        password.encode("utf-8"),
+        bcrypt.gensalt()
+    ).decode("utf-8")
 
-def verify_password(plain, hashed):
-    return pwd_context.verify(plain, hashed)
+def verify_password(
+    plain_password: str,
+    hashed_password: str
+):
+    return bcrypt.checkpw(
+        plain_password.encode("utf-8"),
+        hashed_password.encode("utf-8")
+    )
