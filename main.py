c44fbcfc3efa8550b5878b7b1128787178c8acf4
shortlinke-dotcom <shortlinke@gmail.com>
2026-06-27 10:33:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -549,17 +549,14 @@ async def links(request: Request, login: str = None):
         }
     )
 @app.get("/logout")
-async def logout(request: Request):
-    try:
-        request.session.clear()
-    except:
-        pass
-
+async def logout():
     response = RedirectResponse(
         f"{SUPABASE_URL}/auth/v1/logout?redirect_to=https://earnshortlink.up.railway.app/",
         status_code=303
     )
 
+    response.delete_cookie("sb-access-token")
+    response.delete_cookie("sb-refresh-token")
     response.delete_cookie("access_token")
     response.delete_cookie("refresh_token")
 
