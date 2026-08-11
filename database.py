fadf001a1ecdc4004a6258c57c5d76f0a4eed3e9
shortlinke-dotcom <shortlinke@gmail.com>
2026-08-12 03:17:33 +0700

    Delete database.py

--- database.py
+++
@@ -1,7 +0,0 @@
-from supabase import create_client
-import os
-
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
-
-supabase = create_client(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
